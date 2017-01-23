289a6ba35551b63251bac5d26eeea12193089f91
Jose Campos <alnilak@gmail.com>
2017-01-22 21:18:50 -0500

    First verson of solution to assigment 1

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,42 +8,46 @@ angular.module('LunchCheck', [])
 
 MsgController.$inject = ['$scope'];
 function MsgController($scope) {
-  $scope.name = "Yaakov";
-  $scope.message = "0";
+	$scope.message = "0";
 
-  $scope.sayMessage = function () {
-    return "Yaakov likes to eat healthy snacks at night!";
-  };
+	$scope.sayMessage = function () {
+    	return "Yaakov likes to eat healthy snacks at night!";
+  	};
 
-  $scope.feedYaakov = function () {
-    $scope.stateOfBeing = "fed";
-  };
+  	$scope.feedYaakov = function () {
+    	$scope.stateOfBeing = "fed";
+  	};
 
-  $scope.displayMessage = function () {
-    var message = checkNumber ($scope.menu);
-    $scope.message = message;
-  };
 
-  function checkNumber (values){
-  	console.log ("-------------")
-  	var message = ""
+  	$scope.displayMessage = function () {
+    	var count  = checkNumber ($scope.menu);
+    	var message = "Not valid input "; 
 
-  	if ( values != null ){
-  		if (values.split(",").length > 3 ){
-  			message =  "too much "
-  		}
-  		else {
-  			message = " good enough"
-  		} 
-  	} else {
-  		message = " good enough"
-  	}
+	   
+	   	if  ( count >= 1  &&  count <=3 ) 
+	   	{
+	   		message  = " good enough ... " ;
+	   	} 
+	   	else if ( count > 3 ) 
+	    {
+	    	message  = " too much ... " ;
+	    }
+	    
+	    console.log("asignar valores to msg") ; 
+	    $scope.message = message;
+  	};
 
-  	return message; 
-  }
+
+	function checkNumber (values) {
+  		console.log ("-------------") ; 
+	  	if ( values != null ){
+	  		return ( values.split(",").length )  ;
+	  	} else {
+	  		return 0;
+	  	}
+	}
+
 }
-
-
 
 })();
 
